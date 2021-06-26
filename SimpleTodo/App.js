cb4e06fdce6cadb2293c9d7b93c316526cedf9c8
Jangseongjin <sungjin5891@gmail.com>
2021-06-26 10:51:00 +0900

    추가 : SimpleTodo 프로젝트 레이아웃

--- SimpleTodo/App.js
+++ SimpleTodo/App.js
@@ -1,21 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet} from 'react-native';
+import styled from 'styled-components/native';
+
+const Container = styled.SafeAreaView`
+  flex: 1;
+`;
+
+const Contents = styled.ScrollView`
+  flex: 1;
+  padding: 8px 24px;
+`
+
+const TodoItem = styled.View`
+    flex-direction: row;
+    align-items: center;
+`
+
+const TodoItemText = styled.Text`
+    font-size: 20px;
+    flex: 1;
+`
+
+const TodoItemButton = styled.Button``
+
+const InputContainer = styled.View`
+ flex-direction: row; 
+ padding: 8px 24px;
+`
+
+const Input = styled.TextInput`
+  border: 1px solid #e5e5e5;
+  flex: 1;
+`
+
+const Button = styled.Button``
+
+const TempText = styled.Text`
+    font-size: 20px;
+    margin-bottom: 12px;
+`
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <Container>
+            <Contents>
+                <TodoItem>
+                    <TodoItemText>할 일 목룍 표시</TodoItemText>
+                    <TodoItemButton title="삭제" onPress={() => {}}/>
+                </TodoItem>
+            </Contents>
+            <InputContainer>
+                <Input/>
+                <Button title="전송" onPress={() => {
+                }}/>
+            </InputContainer>
+        </Container>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });

0ae49de03331388c759ffaa75c7ad52c47b90b8d
Jangseongjin <sungjin5891@gmail.com>
2021-06-26 10:14:04 +0900

    Create ImpleTodo Project

--- firstExpoProject/App.js
+++ firstExpoProject/App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import styled from 'styled-components/native';
+
+const BigText = styled.Text`
+  font-size: 24px;
+  font-weight: bold;
+`
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <BigText>Hell World!</BigText>
       <StatusBar style="auto" />
     </View>
   );

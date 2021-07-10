461afdfec07e432232cea6a8bb39a5ab05404482
Jangseongjin <sungjin5891@gmail.com>
2021-07-10 12:25:52 +0900

    추가 : 업로드 예제

--- UploadDemo/App.js
+++ UploadDemo/App.js
@@ -6,107 +6,86 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import type {Node} from 'react';
+import React, { useState } from "react";
+import type { Node } from "react";
 import {
+  ActivityIndicator,
+  Button, Image,
   SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+  StatusBar, Text, View,
+} from "react-native";
+import ImagePicker from "react-native-image-picker";
+import axios from "axios";
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+const options = {
+  title: "이미지를 선택해주세요.",
+  customButtons: [{ name: "fb", title: "페이스북 사진첩에서 서택하기" }],
+  storageOptions: {
+    skipBackup: true,
+    path: "images",
+  },
 };
+
+const config = {
+  headers: {
+    Authorization: 'Client-ID a13f17697adf290'
+  }
+}
 
 const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [url, setUrl] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <Button title="이미지 업로드" onPress={() => {
+          setIsLoading(true)
+          ImagePicker.showImagePicker(options, (response) => {
+            if (response.didCancel) {
+              setIsLoading(false)
+              console.warn("User cancelled image picker");
+            } else if (response.error) {
+              setIsLoading(false)
+              console.warn("ImagePicker Error: ", response.error);
+            } else if (response.customButton) {
+              setIsLoading(false)
+              console.warn("User tapped custom button: ", response.customButton);
+            } else {
+              // You can also display the image using data:
+              // setUrl("data:" + response.type + ";base64," + response.data);
+
+              const params = new FormData();
+              params.append('image', response.data)
+              axios.post('https://api.imgur.com/3/upload', params, config)
+                .then((response) => {
+                  console.log(response.data)
+                })
+                .catch(error => {
+                  console.log(error)
+                })
+                .finally(() => {
+                  setIsLoading(false)
+                })
+            }
+          });
+        }} />
+        {isLoading ? (<ActivityIndicator />) : (
+          <>
+            {url && (
+              <Image source={{ uri: url }} style={{ width: 340, height: 340 }} />
+            )}
+          </>
+        )}
+
+      </SafeAreaView>
+    </>
+
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
